b01b7f920c44414aef7ff0c361faf44988bb5463
ranjanadevi1802 <ranjanadevi180264@gmail.com>
2026-09-08 22:46:22 +0530

    reviewed changes

--- src/App.jsx
+++ src/App.jsx
@@ -744,7 +744,7 @@ function App() {
                           <div className="timeline-badge-group left-align">
                             <span className="timeline-year">2023 - Present (Exp. 2027)</span>
                             <div className="timeline-circle-badge orange-theme">
-                              <img src={asset('/Images/wec.jpg')} alt="Women's Engineering College" style={{ width: '100%', height: '100%', objectFit: 'contain', borderRadius: '50%' }} />                            </div>
+                              <img src={asset('/Images/wec.jpg')} alt="Women's Engineering College" style={{ width: '150%', height: '150%', objectFit: 'contain', borderRadius: '50%' }} />                            </div>
                             <div className="timeline-connector-line"></div>
                           </div>
                           <div className="timeline-center-node orange-theme">
